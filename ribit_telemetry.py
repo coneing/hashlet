d9cfcf5f511ee8c746547641ae335c98d7718f2a
_ <todstikles@gmail.com>
2025-10-07 13:30:15 +0900

    Update ribit_telemetry.py

--- ribit_telemetry.py
+++ ribit_telemetry.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 # ribit_telemetry.py - 3D Ribit structure with green curves, extruded arms, and rasterization to light simulation.
+# Copyright 2025 Todd Hutchinson, Beau Ayres, Anonymous
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Proprietary Software - All Rights Reserved
+#
+# This software is proprietary and confidential. Unauthorized copying,
+# distribution, modification, or use is strictly prohibited without
+# express written permission from Todd Hutchinson and Beau Ayres.
 # Generates smoothed arms, extrudes along Z, visualizes, saves as PNG, and simulates light raster/hashing (ties to Hashlet optics).
 # Complete; run as-is. Mentally verified: Generates plot, saves PNG, hashes for light sim.
 import numpy as np
