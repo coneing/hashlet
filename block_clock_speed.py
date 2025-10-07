728a5fce9aeb7e3cb808bc690dfe94ff0df11e80
_ <todstikles@gmail.com>
2025-10-07 12:42:56 +0900

    Update block_clock_speed.py

--- block_clock_speed.py
+++ block_clock_speed.py
@@ -36,11 +36,14 @@ import hashlib
 import time
 import numpy as np
 import mpmath
+import multiprocessing as mp
+from queue import Empty
 mpmath.mp.dps = 19
 from kappawise import murmur32, kappa_coord
 from wise_transforms import bitwise_transform, hexwise_transform, hashwise_transform
-from hybrid import HybridGreenText, parse_green_perl, scale_curvature
-# From hybrids (cython as py, perl mock)
+from hybrid import HybridGreenText
+
+# Hybrids (py equiv)
 def compute_phi_kappa(points):
     n = points.shape[0]
     if n < 3:
@@ -55,10 +58,7 @@ def compute_phi_kappa(points):
     phi = float(mpmath.phi)
     for i in range(n-2):
         denom = (dl[i]**2 + dh[i]**2)**1.5
-        if denom == 0:
-            kappa[i] = 0.0
-        else:
-            kappa[i] = abs(dl[i] * d2h[i] - dh[i] * d2l[i]) / denom * phi
+        kappa[i] = abs(dl[i] * d2h[i] - dh[i] * d2l[i]) / denom * phi if denom else 0.0
     return np.mean(kappa)
 
 # Hashloop
@@ -70,58 +70,66 @@ def hashloop(start='0', salt=''):
         yield hash_val
         nonce = hash_val
 
-# Mock gossip
-def get_A():
-    return 'mock_prev_hash'
-
-def get_C():
-    time.sleep(0.1)
-    return 'mock_next_hash'
-
-# Main with wise/hybrids
-def block_clock_speed(salt='', lag=0, user_id='blossom'):
+# Node loop for concurrency
+def node_loop(node_id, gossip_queue, salt='', user_id='blossom'):
     generator = hashloop(salt=salt)
     latencies = []
-    coords_accum = []  # For points: use x,y; z for later
-    kappas = []  # Accum kappa means
+    coords_accum = []
+    kappas = []
     hgt = HybridGreenText()
     tick_i = 0
     while True:
-        A = get_A()
+        try:
+            A = gossip_queue.get(timeout=0.05) if tick_i % 2 == 0 else 'mock_prev'
+        except Empty:
+            A = 'mock_prev'
         B = next(generator)
-        C = get_C()
+        try:
+            C = gossip_queue.get(timeout=0.05) if tick_i % 3 == 0 else 'mock_next'
+        except Empty:
+            C = 'mock_next'
         final_input = A + B + C
         final_hash = hashlib.sha256(final_input.encode()).hexdigest()
-        # Wise braid
         bit_out = bitwise_transform(final_hash)
         hex_out = hexwise_transform(final_hash)
         hash_out, ent = hashwise_transform(final_hash)
         hybrid_strand = f"{bit_out}:{hex_out}:{hash_out}"
-        # Kappa coord
-        coord = kappa_coord(user_id, tick_i)
-        coords_accum.append(coord[:2])  # x,y as l,h
+        coord = kappa_coord(user_id + str(node_id), tick_i)
+        coords_accum.append(coord[:2])
         if len(coords_accum) > 2:
             points = np.array(coords_accum)
             kappa_mean = compute_phi_kappa(points)
             kappas.append(kappa_mean)
             scaled = hgt.scale_curvature(np.array(kappas))
-            interval = scaled[-1] / 10.0  # Norm to sec
+            interval = scaled[-1] / 10.0
+            vibe_drag = 1 + (kappa_mean / 10)  # Friction vibe
+            interval *= vibe_drag
         else:
             interval = 0.1
-        # Green log
-        log_text = f"> Tick {tick_i}: {hybrid_strand[:16]}... at {coord} (ent {ent})"
+        log_text = f"> Node {node_id} Tick {tick_i}: {hybrid_strand[:16]}... at {coord} (ent {ent})"
         parsed = hgt.parse_green_perl(log_text)
         print(parsed or log_text)
-        # Latency
         start = time.time()
         receipt_time = time.time() - start + np.random.uniform(0.05, 0.15)
         latencies.append(receipt_time)
         if len(latencies) > 10:
             latencies = latencies[-10:]
         median_c = np.median(latencies)
-        print(f'Median c: {median_c}')
-        time.sleep(max(interval, 0.05))  # Min sleep
+        print(f'Node {node_id} Median c: {median_c}')
+        gossip_queue.put(final_hash)  # Broadcast to fleet
+        time.sleep(max(interval, 0.05))
         tick_i += 1
 
+# Fleet sim
+def block_clock_speed_fleet(nodes=4, salt=''):
+    gossip_queue = mp.Queue()
+    processes = []
+    for i in range(nodes):
+        p = mp.Process(target=node_loop, args=(i, gossip_queue, salt))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
+
 if __name__ == '__main__':
-    block_clock_speed(salt='blossom')
+    block_clock_speed_fleet(salt='blossom')
