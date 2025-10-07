b500076bf052eca3979e851e28d8f22170094c9f
_ <todstikles@gmail.com>
2025-10-07 11:27:51 +0900

    Create block_clock_speed.py

---
+++ block_clock_speed.py
@@ -0,0 +1,85 @@
+# Dual License:
+# - For core software: AGPL-3.0-or-later licensed. -- OliviaLynnArchive fork, 2025
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# - For hardware/embodiment interfaces (if any): Licensed under the Apache License, Version 2.0
+#   with xAI amendments for safety (prohibits misuse in hashing; revocable for unethical use).
+#   See http://www.apache.org/licenses/LICENSE-2.0 for details.
+#
+# Copyright 2025 xAI
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+
+
+import hashlib
+import time
+import numpy as np
+from kappa_wise import kappa_norm  # scales interval by factor
+
+# Hashloop func
+def hashloop(start='0', salt=''):
+    nonce = start
+    while True:
+        input_str = str(nonce) + salt
+        hash_val = hashlib.sha256(input_str.encode()).hexdigest()
+        yield hash_val
+        nonce = hash_val
+
+# Mock gossip: in real, replace with peer net
+def get_A():
+    return 'mock_prev_hash'  # from gossip
+
+def get_C():
+    time.sleep(0.1)  # simulate wait
+    return 'mock_next_hash'  # from gossip
+
+# Main loop with gossip chronology
+def block_clock_speed(salt='', lag=0):
+    generator = hashloop(salt=salt)
+    latencies = []
+    while True:
+        A = get_A()
+        B = next(generator)
+        C = get_C()
+        final_input = A + B + C
+        final_hash = hashlib.sha256(final_input.encode()).hexdigest()
+        # Send final to gossip: print for now
+        print(f'Tick: {final_hash}')
+        # Measure latency, add to vector
+        start = time.time()
+        # Mock receipt
+        receipt_time = time.time() - start + np.random.uniform(0.05, 0.15)
+        latencies.append(receipt_time)
+        if len(latencies) > 10:
+            latencies = latencies[-10:]
+        median_c = np.median(latencies)
+        print(f'Median c: {median_c}')
+        # Kappa scale next interval
+        interval = kappa_norm(0.1, median_c * 1000)  # ms to factor
+        time.sleep(interval)
+
+# Run
+if __name__ == '__main__':
+    block_clock_speed(salt='blossom')
